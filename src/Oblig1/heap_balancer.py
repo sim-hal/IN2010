d22ea0536fca4bc453487aba4bcd4e10bfedb153
severin-sch <schirmersev@gmail.com>
2021-09-16 14:45:31 +0200

    Implemented balancer with heaps. It's tested with the BalanceChecker and works well. Might be optimized further?

---
+++ src/Oblig1/heap_balancer.py
@@ -0,0 +1,38 @@
+from heapq import heappush, heappop
+import sys
+
+
+def heap_balancer(heap):
+    """Print out the values in a heap in order to achieve a binary sorted tree"""
+    if len(heap) == 1:
+        sys.stdout.write(f"{heappop(heap)}\n")
+    if len(heap) == 2:
+        a = heappop(heap)
+        sys.stdout.write(f"{heappop(heap)}\n")
+        sys.stdout.write(f"{a}\n")
+    else:
+        left = []
+        right = []
+
+        heap_len = len(heap)
+
+        for _ in range(heap_len//2):
+            l = heappop(heap)
+            heappush(left, l)
+
+        sys.stdout.write(f"{heappop(heap)}\n")
+
+        for _ in range(heap_len//2 - 1 + heap_len % 2):
+            r = heappop(heap)
+            heappush(right, r)
+
+        heap_balancer(left)
+        heap_balancer(right)
+
+
+if __name__ == "__main__":
+    lines = sys.stdin.readlines()
+    h = []
+    for line in lines:
+        heappush(h, int(line))
+    heap_balancer(h)
