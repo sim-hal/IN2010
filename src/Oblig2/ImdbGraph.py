0cb59eb73a2b520c25a687a3dc724a49b3ddc87b
sim-hal <simon.halstensen@gmail.com>
2021-10-11 12:34:55 +0200

    Introduced Path dataclass, and fixed Dijkstra

--- src/Oblig2/ImdbGraph.py
+++ src/Oblig2/ImdbGraph.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import AsyncGenerator, DefaultDict, Dict, List
+from typing import AsyncGenerator, DefaultDict, Dict, List, Tuple
 from dataclasses import dataclass
 from itertools import combinations
 from collections import deque
@@ -10,6 +10,11 @@ class Movie:
     title: str
     rating: float
 
+@dataclass
+class Path:
+    cost: float
+    nodes: List[Actor]
+
 class Actor:
     def __init__(self, nm_id, name) -> None:
         self.nm_id = nm_id
@@ -19,17 +24,14 @@ class Actor:
     
     def link_to(self, other: Actor, movie: Movie):
         self.movies[other].append(movie)
-
-    def __lt__(self, other):
-        #pass?
-        return True
     
-    #def __le__(self, other): 
+    def __lt__(self, other):
+        return self.name < other.name 
 
     # TODO: Tror vi kan pushe actor (klasse) til heap, problemet er bare når to rating er like vil den gå videre i
     # tuplet og sammenlikne actor med actor. Tror det kan fikses med å sette < til def __lt__ i actor
     @property
-    def sorted_movies(self):
+    def sorted_movies(self) -> List[Tuple[float, Actor, Movie]]:
         if self._neighbours_heapq is not None:
             return self._neighbours_heapq
         self._neighbours_heapq = []
@@ -115,23 +117,23 @@ class IMDbGraph:
     def chillest_path(self, start_id: str, end_id: str):
         start = self.vertices[start_id]
         end = self.vertices[end_id]
-        heapq = [(0, start)]
-        paths: Dict[Actor, List] = {start: [0, []], end: [float("inf"), []]}
+        heapq: List[Tuple[float, Actor]] = [(0, start)]
+        paths: Dict[Actor, Path] = {start: Path(0, []), end: Path(float("inf"), [])}
         #print(paths[start][0], "----------")
         while len(heapq) != 0:
-            (c_w, c_actor) = heappop(heapq)       # c is for 'current'
-            for (w, actor, movie) in c_actor.sorted_movies:  #føkker actor seg opp her? Er ikke et Actor objekt lenger men et list object
+            (c_w, c_actor) = heappop(heapq)
+            for (w, actor, movie) in c_actor.sorted_movies: 
                 # If we have not seen this actor before add them to the paths and heap
                 if actor not in paths:
-                    paths[actor] = [paths[c_actor][0] + w, [paths[c_actor][1]+[c_actor]]]
-                    #print(paths[actor][0])
-                    heappush(heapq, (paths[actor][0], actor))
+                    c_path = paths[c_actor]
+                    paths[actor] = Path(c_path.cost + w, c_path.nodes+[c_actor])
+                    heappush(heapq, (paths[actor].cost, actor))
                 # If we have seen this actor before compare them to the current path
-                elif paths[actor][0] > paths[c_actor][0] + w:
-                    paths[actor] = [paths[c_actor][0] + w, [paths[c_actor][1] + [c_actor]]]
-                    heappush(heapq, (paths[actor][0], actor))
+                elif paths[actor].cost > paths[c_actor].cost + w:
+                    paths[actor] = Path(paths[c_actor].cost + w, paths[c_actor].nodes + [c_actor])
+                    heappush(heapq, (paths[actor].cost, actor))
 
-        final_path = paths[end] + [end]
+        final_path = paths[end].nodes + [end]
 
         print(f"\n{start.name}")
         for i, actor in enumerate(final_path[1:]):
