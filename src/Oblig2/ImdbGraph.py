cc8baad4cecc53e8b63c5be1c7bc965d51bbc7af
sim-hal <simon.halstensen@gmail.com>
2021-10-12 13:38:34 +0200

    Separating Path from WeightedPath, added a defauldict insted of in check, and refactor

--- src/Oblig2/ImdbGraph.py
+++ src/Oblig2/ImdbGraph.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import AsyncGenerator, DefaultDict, Dict, List, Tuple, Set
+from typing import DefaultDict, Dict, List, Tuple, Set, Optional
 from dataclasses import dataclass
 from itertools import combinations
 from collections import deque
@@ -10,17 +10,41 @@ class Movie:
     title: str
     rating: float
 
+
 @dataclass
 class Path:
-    cost: float
     nodes: List[Actor]
+    edges: List[Movie]
+    
+    def __iter__(self, ):
+        self.n = 0
+        return self
+
+    def __next__(self) -> Tuple[Actor, Movie]:
+        if self.n < len(self.edges):
+            diff = len(self.nodes) - len(self.edges) # diff should be 0 or 1
+            res = self.nodes[self.n + diff], self.edges[self.n]
+            self.n += 1
+            return res
+        else:
+            raise StopIteration
+    
+    def pure_append(self, node: Actor, edge: Movie) -> Path:
+        return Path(self.nodes + [node], self.edges + [edge])
+    
+@dataclass
+class WeightedPath(Path):
+    cost: float
+    
+    def pure_append(self, node: Actor, edge: Movie, cost: float) -> WeightedPath:
+        return WeightedPath(self.nodes + [node], self.edges + [edge], self.cost + cost)
 
 class Actor:
     def __init__(self, nm_id, name) -> None:
         self.nm_id = nm_id
         self.name = name
         self.movies = DefaultDict[Actor, List[Movie]](list)
-        self._neighbours_heapq = None
+        self._neighbours_heapq: Optional[List[Tuple[float, Actor, Movie]]] = None
     
     def link_to(self, other: Actor, movie: Movie):
         self.movies[other].append(movie)
@@ -30,8 +54,10 @@ class Actor:
 
     @property
     def sorted_movies(self) -> List[Tuple[float, Actor, Movie]]:
-        if self._neighbours_heapq is not None:
-            return self._neighbours_heapq
+        return self._neighbours_heapq if self._neighbours_heapq is not None else self._fill_neighbours_heapq()
+    
+    
+    def _fill_neighbours_heapq(self) -> List[Tuple[float, Actor, Movie]]:
         self._neighbours_heapq = []
 
         for actor in self.movies: 
@@ -43,7 +69,6 @@ class Actor:
                     if movie.rating > best.rating:
                         best = movie
                 heappush(self._neighbours_heapq, (10-best.rating, actor, best))
-        
         return self._neighbours_heapq
 
 def read_movies(moviesTsv):
@@ -86,66 +111,60 @@ class IMDbGraph:
     def count_vertices_and_edges(self) -> None:
         v = len(self.vertices)
         e = sum(len(ms) for a in self.vertices.values() for ms in a.movies.values()) // 2
-        print(f"Oppgave 1\n\n{v} \n{e}\n")
+        print(f"{v} \n{e}\n")
 
     def unweighted_shortest_path(self, start_id: str, end_id: str):
         start = self.vertices[start_id]
         end = self.vertices[end_id]
         queue = deque([start])
-        paths: Dict[Actor, List[Actor]] = {start: []}
+        paths: Dict[Actor, Path] = {start: Path([], [])}
         visited = []
         while end not in paths:
             current = queue.popleft()
             for neighbour in current.movies.keys():
                 if neighbour not in visited and neighbour not in queue:
-                    paths[neighbour] = paths[current] + [current]
+                    paths[neighbour] = paths[current].pure_append(current, current.movies[neighbour][0])
                     queue.append(neighbour)
                     if neighbour == end:
                         break
             visited.append(current)
-        final_path = paths[end] + [end]
+        final_path = Path(paths[end].nodes + [end], paths[end].edges)
 
         print(f"\n{start.name}")
-        for i, actor in enumerate(final_path[1:]):
-            film = actor.movies[final_path[i]][0]
-            print(f"=== [ {film.title} {film.rating} ] ===> {actor.name}")
+        for actor, movie in final_path:
+            print(f"=== [ {movie.title} {movie.rating} ] ===> {actor.name}")
 
     def chillest_path(self, start_id: str, end_id: str):
         start = self.vertices[start_id]
         end = self.vertices[end_id]
         heapq: List[Tuple[float, Actor]] = [(0, start)]
-        paths: Dict[Actor, Path] = {start: Path(0, []), end: Path(float("inf"), [])}
-        while len(heapq) != 0:
+        paths = DefaultDict[Actor, WeightedPath](lambda: WeightedPath([], [], float("inf")))
+        paths[start] = WeightedPath([], [], 0)
+        while heapq:
             (c_w, c_actor) = heappop(heapq)
             for (w, actor, movie) in c_actor.sorted_movies: 
-                # If we have not seen this actor before add them to the paths and heap
-                if actor not in paths:
+                if paths[actor].cost > paths[c_actor].cost + w:
                     c_path = paths[c_actor]
-                    paths[actor] = Path(c_path.cost + w, c_path.nodes+[c_actor])
-                    heappush(heapq, (paths[actor].cost, actor))
-                # If we have seen this actor before compare them to the current path
-                elif paths[actor].cost > paths[c_actor].cost + w:
-                    paths[actor] = Path(paths[c_actor].cost + w, paths[c_actor].nodes + [c_actor])
+                    paths[actor] = c_path.pure_append(c_actor, movie, w)
                     heappush(heapq, (paths[actor].cost, actor))
 
-        final_path = paths[end].nodes + [end]
+        final_path = WeightedPath(paths[end].nodes + [end], paths[end].edges, paths[end].cost)
 
         print(f"\n{start.name}")
-        for i, actor in enumerate(final_path[1:]):
-            film = actor.movies[final_path[i]][0]
-            print(f"=== [ {film.title} {film.rating} ] ===> {actor.name}")
+        for actor, movie in final_path:
+            print(f"=== [ {movie.title} {movie.rating} ] ===> {actor.name}")
+        print(f"Total cost: {final_path.cost :1f}")
 
     def component_dfs(self):
         v_count = len(self.vertices)
         visited: Set[Actor] = set()
         unvisited: Set[str] = set(self.vertices.keys())
-        component_sizes = {}
+        component_sizes = DefaultDict[int, int](int)
         while len(visited) < v_count:
             count = len(visited)
             current = self.vertices[unvisited.pop()]
             stack: List[Actor] = [current]
-
-            while len(stack) != 0:
+            while stack:
                 current = stack[-1]
                 for neighbour in current.movies:
                     if neighbour.nm_id not in visited:
@@ -157,31 +176,27 @@ class IMDbGraph:
                     visited.add(current.nm_id)
 
             component_size = len(visited) - count
-            if component_size not in component_sizes:
-                component_sizes[component_size] = 1
-            else:
-                component_sizes[component_size] += 1
-
+            component_sizes[component_size] += 1
             unvisited = unvisited - visited
 
-        # TODO: Nice way to print them in order?
-        for size in component_sizes:
+        for size in sorted(component_sizes):
             print(f"There are {component_sizes[size]} components of size {size}")
 
 
 if __name__ == "__main__":
     graph = IMDbGraph("input/movies.tsv", "input/actors.tsv")
+    print("Oppgave  1\n")
     graph.count_vertices_and_edges()
 
     print("Oppgave 2\n")
-    # graph.unweighted_shortest_path("nm2255973", "nm0000460")
-    # graph.unweighted_shortest_path("nm0424060", "nm0000243")
-    # graph.unweighted_shortest_path("nm4689420", "nm0000365")
-    # graph.unweighted_shortest_path("nm0000288", "nm0001401")
-    # graph.unweighted_shortest_path("nm0031483", "nm0931324")
+    graph.unweighted_shortest_path("nm2255973", "nm0000460")
+    graph.unweighted_shortest_path("nm0424060", "nm0000243")
+    graph.unweighted_shortest_path("nm4689420", "nm0000365")
+    graph.unweighted_shortest_path("nm0000288", "nm0001401")
+    graph.unweighted_shortest_path("nm0031483", "nm0931324")
 
     print("Oppgave 3\n")
-    #graph.chillest_path("nm0031483", "nm0931324")
+    graph.chillest_path("nm0031483", "nm0931324")
 
     print("Opggave 4\n")
     graph.component_dfs()
