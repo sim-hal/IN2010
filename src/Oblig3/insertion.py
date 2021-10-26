cb3baf15ff2e53d84c068737c9392c0af8db52cd
severin-sch <schirmersev@gmail.com>
2021-10-26 13:26:54 +0200

    Implemented insertion sort

--- src/Oblig3/insertion.py
+++ src/Oblig3/insertion.py
@@ -1,8 +1,8 @@
+
 def sort(A):
-    # Do insertion sort here. Use the Sorter's comparison- and swap
-    # methods for automatically counting the swaps and comparisons.
-
-    # Use A.swap(i, j) to swap the values at two indices i and j. The swap is
-    # counted, when using this method. Comparisons are counted automatically.
-
+    for i in range(1, len(A)):
+        j = i
+        while j > 0 and A[j-1] > A[j]:
+            A.swap(j, j-1)
+            j -= 1
     return A
