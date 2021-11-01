e2cd605fae3bf3d9b41e236183cf057a57df76a2
severin-sch <schirmersev@gmail.com>
2021-11-01 15:49:00 +0100

    Implemented selection sort

---
+++ src/Oblig3/selection.py
@@ -0,0 +1,13 @@
+from typing import List
+from countswaps import CountSwaps
+
+
+def sort(A: CountSwaps):
+    for i in range(len(A)-1):
+        k = i
+        for j in range(i+1, len(A)-1):
+            if A[j] < A[k]:
+                k = j
+        if i != k:
+            A.swap(i, k)
+    return A
