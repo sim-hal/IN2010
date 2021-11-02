b35cd74309c9cf9190109767c9609cb5a99a9b17
sim-hal <simon.halstensen@gmail.com>
2021-11-02 14:21:33 +0100

    Added typing

--- src/Oblig3/countswaps.py
+++ src/Oblig3/countswaps.py
@@ -1,6 +1,8 @@
-class CountSwaps(list):
-    swaps = 0
+from typing import List
+from countcompares import CountCompares
 
+class CountSwaps(List[CountCompares]):
+    swaps = 0
     def swap(self, i, j):
         self.swaps += 1
         self[i], self[j] = self[j], self[i]
