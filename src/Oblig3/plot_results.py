af605f6c71f02f1be5af4731b6fab9c9a1108c62
sim-hal <simon.halstensen@gmail.com>
2021-11-03 11:10:42 +0100

    fixed mistake in quick sort

--- src/Oblig3/plot_results.py
+++ src/Oblig3/plot_results.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 
 def plot_results(filename: str):
     df = pd.read_csv(filename)
-    fig, axs = plt.subplots(3, 1)
+    fig, axs = plt.subplots(3, 1, sharex=True)
     n = df.index
     for column_name in df.columns[1:]:
         column = df[column_name]
         idx = 0 if "time" in column_name else 1 if "cmp" in column_name else 2
         axs[idx].plot(n[:len(column)], column, label=column_name.split("_")[0])
-    plt.legend()
-    plt.savefig("plot_results.pdf")
+        print(column_name, len(column))
+    #fig.legend()
+    #plt.show()
+    plt.savefig("inputs/plot_results.pdf")
     plt.show()
 
 if __name__ == "__main__":
