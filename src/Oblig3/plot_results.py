6ebf4ef2cef898c932f57e795b1cd6022afb123b
severin-sch <schirmersev@gmail.com>
2021-11-03 11:51:31 +0100

    Updated plotting

--- src/Oblig3/plot_results.py
+++ src/Oblig3/plot_results.py
@@ -20,18 +20,33 @@ def plot_results(filename: str, title: str = ""):
     df = clean_df(df)
     fig, axs = plt.subplots(1, 3, figsize=(10,5), dpi=300)
     n = df.index
+
+    ymax = 0
+
     for column_name in df.columns[1:]:
         column = df[column_name]
         idx = 0 if "time" in column_name else 1 if "cmp" in column_name else 2
-        axs[idx].plot(n[:len(column)], column, label=column_name.split("_")[0])
+        if idx > 0:
+            axs[idx].plot(n[:len(column)], column)
+        if not idx:
+            axs[idx].plot(n[:len(column)], column, label=column_name.split("_")[0])
+            if column.max() > ymax: ymax = column.max()
+
+    axs[0].plot(n, n**2, label=r"$O(n^2)$")
+    axs[0].plot(n, n*np.log(n), label=r"$O(nlog(n))$")
+
+    axs[0].set_ylim(-10000, ymax+10000)
     axs[0].set_title("Time")
     axs[1].set_title("Comparisons")
     axs[2].set_title("Swaps")
-    axs[0].set_ylabel("Time")
+    axs[0].set_ylabel("ms")
     axs[1].set_ylabel("Comparisons")
     axs[2].set_ylabel("Swaps")
+
     for ax in axs: ax.set_xlabel("n")
-    plt.legend()
+
+    fig.legend(bbox_to_anchor=(0.98,0.85), loc="upper right")
+
     fig.suptitle(title)
     filename = filename.split("/")[1].split(".")[0]+".png"
     fig.tight_layout()
@@ -40,5 +55,5 @@ def plot_results(filename: str, title: str = ""):
 
 
 if __name__ == "__main__":
-    plot_results("inputs/random_10000_results.csv", "Input: Random 1000")
+    plot_results("inputs/nearly_sorted_10000_results.csv", "Input: Random 10000")
 
